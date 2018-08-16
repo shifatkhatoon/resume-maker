590000c83f7f05d61949a0571b0f2e1deb5e45e2
shifat1997 <shifatkhatoon1997@gmail.com>
2018-08-16 23:11:44 +0530

    first commit

---
+++ public/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var template = React.createElement(
+  "p",
+  null,
+  "This is a paragarph"
+);
+
+ReactDOM.render(template, document.getElementById("root"));
+console.log("hello");
+console.log("hello")
---
+++ source/app.js
@@ -0,0 +1,4 @@
+
+var template = <p>This is a paragarph</p>
+
+ReactDOM.render(template, document.getElementById("root"));
